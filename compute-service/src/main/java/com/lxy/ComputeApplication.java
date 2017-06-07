0f092ee026eb032f33bc30791270db4cb791acbf
coldxiangyu <coldxiangyu@qq.com>
2017-06-07 13:42:11 +0800

    Initial commit

---
+++ compute-service/src/main/java/com/lxy/ComputeApplication.java
@@ -0,0 +1,16 @@
+package com.lxy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by coldxiangyu on 2017/6/6.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+    }
+}
---
+++ eureka-server/src/main/java/com/lxy/application/Application.java
@@ -0,0 +1,16 @@
+package com.lxy.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by coldxiangyu on 2017/6/6.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
