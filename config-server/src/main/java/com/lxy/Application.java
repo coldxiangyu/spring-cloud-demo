701671e3b9653365ecb6192722c1234b6a4b7200
coldxiangyu <coldxiangyu@qq.com>
2017-06-12 15:48:57 +0800

    config-server

---
+++ config-server/src/main/java/com/lxy/Application.java
@@ -0,0 +1,16 @@
+package com.lxy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by coldxiangyu on 2017/6/9.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
